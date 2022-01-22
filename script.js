6ff6e144c38dea7c7fd483b55ef2ae2385ea8d4b
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-01-22 18:55:18 +0530

    Finished the Cat-Ting project

---
+++ script.js
@@ -0,0 +1,53 @@
+const imgContainer = document.querySelector('.image-container');
+const loader = document.querySelector('.loader');
+
+let ready = false;
+let imagesLoaded = 0;
+let photoArray = [];
+let count = 10;
+
+function imageLoaded() {
+	imagesLoaded++;
+	if (imagesLoaded === photoArray.length - 3) {
+		ready = true;
+		loader.hidden = true;
+		imagesLoaded = 0;
+	}
+}
+
+function displayPhotos() {
+	photoArray.forEach((photo) => {
+		let image = document.createElement('img');
+		image.setAttribute('src', photo.url);
+		image.setAttribute('alt', 'cat photo');
+		imgContainer.appendChild(image);
+		image.addEventListener('load', imageLoaded);
+	});
+}
+
+let url = `https://api.thecatapi.com/v1/images/search?limit=${count}&random&order=Desc`;
+// Get photos
+async function getPhotos() {
+	try {
+		const response = await fetch(url);
+		photoArray = await response.json();
+		displayPhotos();
+	} catch (err) {
+		console.error(err);
+	}
+}
+
+// Check to see if scroll is at bottom of page.
+window.addEventListener('scroll', function (e) {
+	if (
+		window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+		ready
+	) {
+		getPhotos();
+		ready = false;
+	}
+});
+
+getPhotos();
+
+// const url = 'https://api.waifu.pics/many/type/category';
